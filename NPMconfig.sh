ed5758e8e64fd45890e52fdcae2aaf5c8480d025
npjones <nicole-anne.paterson-jones@ext.ec.europa.eu>
2024-03-26 13:57:34 +0100

    updated branch for local generation

---
+++ NPMconfig.sh
@@ -0,0 +1,39 @@
+Echo
+
+echo "-------------------------------------------------------------------------"
+
+echo "SETUP NPM PROXY"
+
+npm --version
+
+RESULT=$?
+
+if [[ $RESULT -ne 0 ]]; then
+
+  echo "npm is not available!"
+
+  exit 1
+
+fi
+
+EFORMS_PROXY_URL=http://paterni:J0n3s0055_M44h@proxy-t2-lu.welcome.ec.europa.eu:8012
+
+if [[ -z "${EFORMS_PROXY_URL}" ]]; then
+
+  echo "Expecting environment variable: EFORMS_PROXY_URL !"
+
+  exit 1
+
+fi
+
+npm config delete proxy
+
+npm config set proxy $EFORMS_PROXY_URL
+
+#npm config get proxy
+
+npm config delete https-proxy
+
+npm config set https-proxy $EFORMS_PROXY_URL
+
+#npm config get https-proxy
